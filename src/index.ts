2a07e0fe2ae67b960c0551072c59f7feae6a91d4
khanh <khanhlnfx20736@funix.edu.vn>
2024-10-06 16:55:22 +0700

    config tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import express, { Application, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import connectDB from './shared/database/database';
-import route from './routes/index';
-import fs from 'fs';
-import path from 'path';
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB from "./shared/database/database";
+import route from "./routes/index";
+import fs from "fs";
+import path from "path";
 // const Home = require ('./views/components/Home.vue');
-
 
 dotenv.config();
 const app: Application = express();
@@ -18,8 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Cấu hình EJS là engine template
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "views"));
+app.get("/", (req: Request, res: Response) => {
+  res.render("index", { title: "Express", message: "dmm" });
+});
 
 // Định nghĩa một route
 route(app);
