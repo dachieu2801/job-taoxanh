ea633985fdeeaf346391b90aa0f661de6831ac9f
khanh <khanhlnfx20736@funix.edu.vn>
2024-10-07 02:24:02 +0700

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/views/**/*.{ejs,html}', './src/**/*.{js,ts}'], // Đường dẫn tới tệp EJS và TypeScript
+  content: ["./src/views/**/*.{ejs,html}", "./src/**/*.{js,ts}"], // Đường dẫn tới tệp EJS và TypeScript
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#008060",
+      },
+    },
   },
   plugins: [],
-}
+};
