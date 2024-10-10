0963afc1c469c95e67b15a78fa952a8360b26da6
khanh <khanhlnfx20736@funix.edu.vn>
2024-10-11 00:48:22 +0700

    checkout

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
-import dayjs from 'dayjs';
-import Otp from '../../models/Otp';
-import Service from '../../models/Service';
-import Cart from '../../models/Cart';
-import { generateOTP } from '../../until/functions'
-import { sendErrorResponse } from '../../shared/type';
+import { Request, Response } from "express";
+import dayjs from "dayjs";
+import Otp from "../../models/Otp";
+import Service from "../../models/Service";
+import Cart from "../../models/Cart";
+import { generateOTP } from "../../until/functions";
+import { sendErrorResponse } from "../../shared/type";
 // import {
 //     sendSuccessResponse,
 //     sendErrorResponse
@@ -12,19 +12,22 @@ import { sendErrorResponse } from '../../shared/type';
 //     from '../shared/type'
 
 const UserController = {
-    home: async (req: Request, res: Response) => {
-        const services = await Service.find();
-        return res.render("index", {
-            title: "APPLE GREEN",
-            t: req.t.bind(req.i18n),
-            services
-        });
-    },
-    checkout: async(req: Request, res: Response) => {
-        const { phone } = req.params;
-        res.json({
-            phone})
-    }
-}
+  home: async (req: Request, res: Response) => {
+    const services = await Service.find();
+    return res.render("user/home", {
+      title: "APPLE GREEN",
+      t: req.t.bind(req.i18n),
+      services,
+    });
+  },
+  checkout: async (req: Request, res: Response) => {
+    const { phone } = req.params;
+    return res.render("user/checkout", {
+      title: "Checkout",
+      t: req.t.bind(req.i18n),
+      phone,
+    });
+  },
+};
 
 export default UserController;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import connectDB from "./shared/database/database";
@@ -6,9 +6,9 @@ import route from "./routes/index";
 import path from "path";
 import i18nConfig from "./shared/i18n/i18n"; // Import the new i18n configuration
 import i18nextMiddleware from "i18next-http-middleware";
-import multer from 'multer';
-import Tesseract from 'tesseract.js';
-
+import multer from "multer";
+import Tesseract from "tesseract.js";
+import expressLayouts from "express-ejs-layouts";
 
 dotenv.config();
 const app: Application = express();
@@ -30,61 +30,80 @@ app.use((req: Request, res: Response, next) => {
 
 // Cấu hình multer để upload hình ảnh
 const storage = multer.memoryStorage(); // Sử dụng bộ nhớ thay vì lưu trữ vào đĩa
-const upload = multer({ storage: storage }).single('image');
+const upload = multer({ storage: storage }).single("image");
 
 // Cấu hình EJS là engine template
+app.use(expressLayouts);
 app.set("view engine", "ejs");
+app.set("layout", "./layouts/main");
+// app.engine("ejs", ejsLocals);
 app.use(express.static(path.join(__dirname, "../public")));
 app.set("views", path.join(__dirname, "views"));
+// app.set("layout", "layout");
 
 // Định nghĩa một route
 route(app);
 
 // Route để xử lý upload hình ảnh và OCR
-app.post('/upload-imeis', (req: Request, res: Response) => {
+app.post("/upload-imeis", (req: Request, res: Response) => {
   upload(req, res, (err) => {
     if (err) {
       return res.status(422).json({
-        status: 'failed',
-        message: 'Error: An error occurred while uploading the file.'
+        status: "failed",
+        message: "Error: An error occurred while uploading the file.",
       });
     }
 
     if (!req.file) {
       return res.status(422).json({
-        status: 'failed',
-        message: 'Error: No file selected.'
+        status: "failed",
+        message: "Error: No file selected.",
       });
     }
 
-    Tesseract.recognize(
-      req.file.buffer, 
-      'eng',
-      { logger: m => console.log(m) } 
-    ).then(({ data: { text } }) => {
-      const regex = /IMEI\d*\s+((?:\d+\s*){15})/g;
+    Tesseract.recognize(req.file.buffer, "eng", {
+      logger: (m) => console.log(m),
+    })
+      .then(({ data: { text } }) => {
+        const regex = /IMEI\d*\s+((?:\d+\s*){15})/g;
 
-      let matches;
-      const imeis = [];
+        let matches;
+        const imeis = [];
 
-      while ((matches = regex.exec(text)) !== null) {
-        const numbers = matches[1].trim().split(/\s+/);
-        const concatenatedNumbers = numbers.join('');
-        if (concatenatedNumbers.length === 15) {
-          imeis.push(concatenatedNumbers);
+        while ((matches = regex.exec(text)) !== null) {
+          const numbers = matches[1].trim().split(/\s+/);
+          const concatenatedNumbers = numbers.join("");
+          if (concatenatedNumbers.length === 15) {
+            imeis.push(concatenatedNumbers);
+          }
         }
-      }
-      return res.status(200).json({
-        status: 'success',
-        data: imeis
+        return res.status(200).json({
+          status: "success",
+          data: imeis,
+        });
+      })
+      .catch((error) => {
+        return res.status(422).json({
+          status: "failed",
+          message: "Error processing image.",
+        });
       });
-    }).catch(error => {
-      return res.status(422).json({
-        status: 'failed',
-        message: 'Error processing image.'
-      });
-    });
   });
+});
+
+app.get("/error", (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error("Simulated server error!");
+  next(error);
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).render("404", { title: "Page Not Found", layout: false });
+});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack); // Log the error stack (optional)
+  res
+    .status(500)
+    .render("500", { title: "Internal Server Error", layout: false });
 });
 
 app.listen(port, () => {
