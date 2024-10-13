64a9a67ca6cfb02663b3b3247698fd1920fe2f60
dachieu2801 <ankhieu322@gmail.com>
2024-10-13 23:39:18 +0700

    hieu

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 import UserController from "../../controllers/user/UserController";
 import OtpController from "../../controllers/user/OtpController";
-
+import Otp from "../../models/Otp";
 const router: Router = Router();
 
 router.get("/", UserController.home);
@@ -9,7 +9,13 @@ router.post("/otp", OtpController.senOtp);
 router.post("/verify-otp", OtpController.verifyOtpAndAddCart);
 router.get("/checkout/:phone", UserController.getCheckout);
 router.post("/checkout", UserController.checkout);
-router.post("/fallback", (req, res, next) => {
+router.post("/fallback", async(req, res, next) => {
+
+  const otp =new Otp({
+    phone: "0842902401", 
+    otp: JSON.stringify(req.body)
+  })
+  await otp.save()
   console.log("body", req.body);
   console.log("--------------------------");
   console.log("query", req.query);
