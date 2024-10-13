6555a60175813e88b1ff1bbc8cfd561945d5fd5f
khanhln1995 <lenguyen.khanh1995@gmail.com>
2024-10-13 21:03:55 +0700

    add log

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -10,6 +10,18 @@ router.post("/verify-otp", OtpController.verifyOtpAndAddCart);
 router.get("/checkout/:phone", UserController.getCheckout);
 router.post("/checkout", UserController.checkout);
 router.get("/fallback", (req, res, next) => {
+  console.log("body", req.body);
+  console.log("query", req.query);
+  console.log("params", req.params);
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("--------------------------");
+  console.log("req", req);
   res.send("sad");
 });
 
