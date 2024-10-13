079a14002530c64f1258826d137bc013f44a6be4
khanhln1995 <lenguyen.khanh1995@gmail.com>
2024-10-13 21:39:07 +0700

    asd

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -9,19 +9,14 @@ router.post("/otp", OtpController.senOtp);
 router.post("/verify-otp", OtpController.verifyOtpAndAddCart);
 router.get("/checkout/:phone", UserController.getCheckout);
 router.post("/checkout", UserController.checkout);
-router.get("/fallback", (req, res, next) => {
+router.post("/fallback", (req, res, next) => {
   console.log("body", req.body);
+  console.log("--------------------------");
   console.log("query", req.query);
+  console.log("--------------------------");
   console.log("params", req.params);
   console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("--------------------------");
-  console.log("req", req);
+
   res.send("sad");
 });
 
